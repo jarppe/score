1738531dabeba42913e10b1f33b3d394df6fcbd5
jarppe <jarppe@metosin.fi>
2012-10-03 00:08:23 +0300

    Print incoming requests

--- src/score/server.clj
+++ src/score/server.clj
@@ -7,7 +7,13 @@
 
 (def score-mode (keyword (or (System/getenv "SCORE_MODE") (System/getProperty "score.mode") "dev")))
 
+(defn debug-requests [handler]
+  (fn [request]
+    (println "DEBUG:" (:request-method request) (:uri request))
+    (handler request)))
+
 (defn -main [& args]
   (db/init)
   (poll/start-poller)
+  (server/add-middleware debug-requests)
   (server/start 8082 {:mode score-mode :ns 'score.web}))
