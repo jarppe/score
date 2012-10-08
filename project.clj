0f638339f3d8ff05f3d222cbbf3c66eca8c4199a
jarppe <jarppe@metosin.fi>
2012-10-08 19:15:10 +0300

    Moving to use cljs

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject score "0.1.0-SNAPSHOT"
   :description "Show 4clojure scoring"
+  :source-path "src/clj"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta10" :exclusions [org.clojure/clojure]]
                  [enlive "1.0.1"]
@@ -10,6 +11,12 @@
                  [org.slf4j/slf4j-simple "1.5.10"]]
   :profiles {:dev {:dependencies [[midje "1.4.0" :exclusions [org.clojure/clojure]]]
                    :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}}
+  :cljsbuild {:builds {:main {:source-path "src/cljs"
+                              :compiler {:output-to "resources/public/score.js"
+                                         :optimizations :whitespace
+                                         :pretty-print true
+                                         :print-input-delimiter true}
+                              :notify-command ["growlnotify" "-m"]}}}
   :main score.server
   :repl-options {:init-ns score.server}
   :min-lein-version "2.0.0")
